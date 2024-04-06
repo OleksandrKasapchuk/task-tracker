9dc7fd81c73206af42361667c5555a7b002f8583
sashapre228 <sasha291108@gmail.com>
2024-04-06 13:14:28 +0200

    muskat noob

--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from .models import *
 from django.views.generic import ListView, DetailView, CreateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import *
 
 
 class Index(TemplateView):
-	pass
+	template_name = 'task_tracker/index.html'
 
 
 class TaskListView(ListView):
@@ -20,8 +22,12 @@ class TaskDetailView(DetailView):
 	template_name = "task_tracker/task_details.html"
 
 
-# class TaskCreateView(CreateView):
-# 	model = Task
-# 	template_name = "task_tracker/task_form.html"
-# 	form_class = TaskCreateForm
-# 	success_url = reverse("task_tracker:tasks")
+class TaskCreateView(LoginRequiredMixin, CreateView):
+	model = Task
+	template_name = "task_tracker/task_form.html"
+	form_class = TaskCreateForm
+	success_url = reverse_lazy("task_tracker:task-list")
+
+	def form_valid(self, form):
+		form.instance.creator = self.request.user
+		return super().form_valid(form)
