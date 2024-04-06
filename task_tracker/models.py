838b52ac0eaed296996ce262f9908815c12e3f83
sashapre228 <sasha291108@gmail.com>
2024-04-06 23:12:48 +0200

    popka muskata

--- task_tracker/models.py
+++ task_tracker/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from auth_system.models import CustomUser
 
+
+# class Dashboard(models.Model):
+# 	name = models.CharField(max_length=100)
+
+
 class Task(models.Model):
 
 	STATUS_CHOICES = [
--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
 	path('', task_views.Index.as_view(), name='index'),
 	path('tasks/', task_views.TaskListView.as_view(), name='task-list'),
 	path('tasks/<int:pk>', task_views.TaskDetailView.as_view(), name='task-details'),
-	path('task-create/', task_views.TaskCreateView.as_view(), name='task-create')
+	path('task-create/', task_views.TaskCreateView.as_view(), name='task-create'),
+	path('task-update/<int:pk>', task_views.TaskUpdateView.as_view(), name='task-update'),
+	path('task-delete/<int:pk>', task_views.TaskDeleteView.as_view(), name='task-delete'),
 ]
 
-app_name = "task_tracker"
+app_name = "task-tracker"
