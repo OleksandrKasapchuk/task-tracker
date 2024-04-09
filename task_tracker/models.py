07b8291d916679fe6c3717f0e4c1e7e5d492a0e9
sashapre228 <sasha291108@gmail.com>
2024-04-09 22:09:09 +0200

    hz

--- task_tracker/models.py
+++ task_tracker/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from auth_system.models import CustomUser
 
 
-# class Dashboard(models.Model):
-# 	name = models.CharField(max_length=100)
+class Dashboard(models.Model):
+	name = models.CharField(max_length=100)
+	users = models.ManyToManyField(CustomUser)
 
+	def __str__(self):
+		return self.name
+	
 
 class Task(models.Model):
 
@@ -18,13 +22,14 @@ class Task(models.Model):
 		("medium", "Medium"),
 		("high", "High")
 	]
+
 	name = models.CharField(max_length=50)
 	description = models.TextField()
 	status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="todo")
 	priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default="medium")
 	end_date = models.DateField(null=True, blank=True)
 	creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="tasks")
-
+	dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE, related_name="tasks")
 
 	def __str__(self):
 		return self.name
