f5b9fe6786d64703815fe0d934054eff80f2c57a
OleksandrKasapchuk <105971884+OleksandrKasapchuk@users.noreply.github.com>
2024-04-10 00:41:26 +0300

    Delete auth_system directory

--- auth_system/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-	bio = models.TextField(null=True, blank=True)
-
-	def __str__(self):
-		return self.username
