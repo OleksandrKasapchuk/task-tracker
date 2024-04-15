d3b3b1ed569f74b536cf311a54832a94abce87d6
sashapre228 <sasha291108@gmail.com>
2024-04-15 22:22:10 +0200

    mama comment

--- task_tracker/forms.py
+++ task_tracker/forms.py
@@ -6,3 +6,9 @@ class TaskCreateForm(forms.ModelForm):
 	class Meta:
 		model = Task
 		fields = ["name", "description", "status", "priority", "end_date"]
+
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ["content"]
