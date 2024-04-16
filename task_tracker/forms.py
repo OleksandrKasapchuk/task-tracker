7a18fd219d5509c4e84070da84f2f2e045338e2c
sashapre228 <sasha291108@gmail.com>
2024-04-16 23:13:50 +0200

    priority filter

--- task_tracker/forms.py
+++ task_tracker/forms.py
@@ -3,10 +3,17 @@ from .models import *
 
 
 class TaskCreateForm(forms.ModelForm):
+	end_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local',}))
 	class Meta:
 		model = Task
-		fields = ["name", "description", "status", "priority", "end_date"]
+		fields = ["name", "description", "status", "priority"]
 
+
+class FilterTaskForm(forms.Form):
+	result = [("4", "All")]
+	for i in Task.PRIORITY_CHOICES:
+		result.append(i)
+	priority = forms.ChoiceField(choices=result, required=False, label="Priority")
 
 class CommentForm(forms.ModelForm):
 	class Meta:
--- task_tracker/mixins.py
+++ task_tracker/mixins.py
@@ -6,10 +6,3 @@ class UserIsOwnerMixin(object):
 		if instance.creator != self.request.user:
 			raise PermissionDenied
 		return super().dispatch(request, *args, **kwargs)
-
-
-# class DashboardMixin:
-# 	def get_context_data(self,**kwargs):
-# 		context = super().get_context_data(**kwargs)
-# 		context["dashboard_pk"] = self.kwargs.get("dashboard_pk", None)
-# 		return context
